cfdcacae66f077097a27c0065c1c595462eb6a38
Alejandro Espinosa <alejandrp13@gmail.com>
2023-08-17 17:22:13 -0500

    cambiaso7

--- src/Date/trabajos.js
+++ src/Date/trabajos.js
@@ -19,112 +19,130 @@ export const trabajos = [
       'id':'P2',
       'nombre': 'Punto 2',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 2
      },
    
      {
       'id':'R2',
       'nombre': 'Respuesta 2',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 3
      
      },
      {
       'id':'P3',
       'nombre': 'Pregunta 3',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 4
      },
      {
       'id':'R3',
       'nombre': 'Respuesta 3',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 5
      },
      {
       'id':'P4',
         'nombre': 'Pregunta 4',
         'tecnologias': 'Lenguaje C',
-        'categoria': 'Programacion c'
+        'categoria': 'Programacion c',
+        'img': 6
      },
      {
       'id':'R4',
       'nombre': 'Respuesta 4',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 7
      },
      {
       'id':'P5',
         'nombre': 'Pregunta 5',
         'tecnologias': 'Lenguaje C',
-        'categoria': 'Programacion c'
+        'categoria': 'Programacion c',
+        'img': 8
      },
      {
       'id':'R5',
       'nombre': 'Respuesta 5',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 9
      },
      {
       'id':'P6',
         'nombre': 'Pregunta 6',
         'tecnologias': 'Lenguaje C',
-        'categoria': 'Programacion c'
+        'categoria': 'Programacion c',
+        'img': 10
      },
      {
       'id':'R6',
       'nombre': 'Respuesta 6',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 11
      },
      {
       'id':'P7',
       'nombre': 'Pregunta 7',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 12
      },
      {
       'id':'R7',
       'nombre': 'Respuesta 7',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 13
 
      },
      {
       'id':'P8',
       'nombre': 'Pregunta 8',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 14
      },
      {
       'id':'R8',
       'nombre': 'Respuesta 8',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 15
      },
      {
       'id':'P9',
       'nombre': 'Pregunta 9',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 16
      },
      {
       'id':'R9',
       'nombre': 'Respuesta 9',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 17
      },
      {
       'id':'P10',
       'nombre': 'Pregunta 10',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 18
      },
      {
       'id':'R10',
       'nombre': 'Respuesta 10',
       'tecnologias': 'Lenguaje C',
-      'categoria': 'Programacion c'
+      'categoria': 'Programacion c',
+      'img': 19
      }
 
 
--- src/imagenes/imagen.js
+++ src/imagenes/imagen.js
@@ -1,7 +1,44 @@
 import Punto1 from '../imagenes/Punto_1.PNG'
 import R1 from '../imagenes/R1.PNG';
+import Punto2 from '../imagenes/P2.PNG'
+import R2 from '../imagenes/R2.PNG';
+import Punto3 from '../imagenes/P3.PNG'
+import R3 from '../imagenes/R3.PNG';
+import Punto4 from '../imagenes/P4.PNG'
+import R4 from '../imagenes/R4.PNG';
+import Punto5 from '../imagenes/P5.PNG'
+import R5 from '../imagenes/R5.PNG';
+import Punto6 from '../imagenes/P6.PNG'
+import R6 from '../imagenes/R6.PNG';
+import Punto7 from '../imagenes/P7.PNG'
+import R7 from '../imagenes/R7.PNG';
+import Punto8 from '../imagenes/P8.PNG'
+import R8 from '../imagenes/R8.PNG';
+import Punto9 from '../imagenes/P9.PNG'
+import R9 from '../imagenes/R9.PNG';
+import Punto10 from '../imagenes/P10.PNG'
+import R10 from '../imagenes/R10.PNG';
+
 
 export default[
   Punto1,
-  R1
+  R1,
+  Punto2,
+  R2,
+  Punto3,
+  R3,
+  Punto4,
+  R4,
+  Punto5,
+  R5,
+  Punto6,
+  R6,
+  Punto7,
+  R7,
+  Punto8,
+  R8,
+  Punto9,
+  R9,
+  Punto10,
+  R10
 ]
