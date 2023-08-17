7d64437426a85ac312bdb7dd44290caf41511a2d
Alejandro Espinosa <alejandrp13@gmail.com>
2023-08-17 15:48:07 -0500

    cambiaso4

--- public/imagenes/imagen.js
+++ public/imagenes/imagen.js
@@ -1,5 +1,5 @@
-import Punto1 from './img/Punto_1.PNG';
-import R1 from './img/R1.PNG';
+import Punto1 from './imagenes/Punto_1.PNG';
+import R1 from './imagenes/R1.PNG';
 
 const images = {
   "P1": Punto1,
